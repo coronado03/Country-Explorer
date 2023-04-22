e41bb38cce1adc087d684f6bd47c28a019eaeac8
coronado03 <sebascoronado03@hotmail.com>
2023-04-22 14:37:41 +0200

    Added basic layout and UI to cardComponent

--- components/countryCard.tsx
+++ components/countryCard.tsx
@@ -10,8 +10,15 @@ type Props = {
 
 const CountryCard: React.FC<Props> = ({countryName, countryRegion, countryCapital, countryLanguages, countryCurrencies }) => {
   return (
-    <>
-    </>
+      <div className='flex flex-col mt-5 border-4 border-[#21A179] shadow-md rounded-lg gap-5 w-full'>
+        <div className='text-center mt-5'>
+          <h1 className='text-5xl'>{countryName}</h1>
+          <h5 className='text-xl'>{countryRegion}</h5>
+        </div>
+        <p><b>Capital: </b>{countryCapital}</p>
+        <p><b>Language: </b>{countryLanguages}</p>
+        <p className='mb-5'><b>Currencies: </b>{countryCurrencies}</p>
+      </div>
   );
 };
 
