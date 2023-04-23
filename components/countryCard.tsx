8e047a97ee63447209d78c2c7aece86a923ad7b9
coronado03 <sebascoronado03@hotmail.com>
2023-04-23 15:39:07 +0200

    Initial countryCardComponentRework commit

--- components/countryCard.tsx
+++ components/countryCard.tsx
@@ -2,22 +2,30 @@ import React from 'react';
 
 type Props = {
   countryName: string;
+  countrySubregion: string;
+  countryPopulation: number;
   countryRegion: string;
   countryCapital: string;
   countryLanguages: string[];
   countryCurrencies: string[];
+  countryFlag: string;
 };
 
-const CountryCard: React.FC<Props> = ({countryName, countryRegion, countryCapital, countryLanguages, countryCurrencies }) => {
+const CountryCard: React.FC<Props> = ({countryName, countryRegion, countrySubregion, countryPopulation, countryCapital, countryLanguages, countryCurrencies, countryFlag }) => {
   return (
-      <div className='flex flex-col mt-5 border-4 border-[#21A179] shadow-md rounded-lg gap-5 w-full'>
-        <div className='text-center mt-5'>
+      <div className='flex flex-col mt-5 border-4 border-black shadow-md rounded-lg gap-5 w-full'>
+        <div className='flex flex-col text-center mt-5'>
           <h1 className='text-5xl'>{countryName}</h1>
-          <h5 className='text-xl'>{countryRegion}</h5>
+          <img className='w-1/2 mt-4 self-center' src={countryFlag}></img>
+          <h5 className='text-xl'>{countryRegion}, {countrySubregion}</h5>
         </div>
-        <p><b>Capital: </b>{countryCapital}</p>
-        <p><b>Language: </b>{countryLanguages}</p>
-        <p className='mb-5'><b>Currencies: </b>{countryCurrencies}</p>
+        <div className='mb-5 mx-auto'>   
+          <p><b>Capital: </b>{countryCapital}</p>
+          <p><b>Population: </b>{countryPopulation.toLocaleString()}</p> 
+          <p><b>Language: </b>{countryLanguages}</p>
+          <p><b>Currencies: </b>{countryCurrencies}</p>
+        </div>
+
       </div>
   );
 };
