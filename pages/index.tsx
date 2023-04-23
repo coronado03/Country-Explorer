dcaf621193332e91e05677cf886cc525720b47dd
coronado03 <sebascoronado03@hotmail.com>
2023-04-23 11:33:12 +0200

    Initial restCountriesHandler Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,62 @@
-import { useQuery } from '@apollo/client';
-import { GET_COUNTRIES_BY_CONTINENT } from '@/lib/countriesQueries';
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_COUNTRIES_BY_CONTINENT } from "@/lib/countriesQueries";
 
+type Country = {
+  alpha2Code: string;
+  name: string;
+  capital: string;
+  population: number;
+  flag: string;
+};
 
-export default function Home() {
+const Home = () => {
+  const [continent, setContinent] = useState<string>("");
+  const [numCountries, setNumCountries] = useState<number>(2);
+  const [countryDetails, setCountryDetails] = useState<Country[]>([]);
 
-  const { loading, error, data } = useQuery(GET_COUNTRIES_BY_CONTINENT, {
+  //Using the useQuery hook we get the needed data, passing continent as a parameter
+  const { data } = useQuery(GET_COUNTRIES_BY_CONTINENT, {
     variables: { continent },
   });
 
-  const shuffledCountries = [...data.countries].sort(() => 0.5 - Math.random()); //Shuffles the countries from the continent
-  const countries = shuffledCountries.slice(0, 5); //The number 5 will eventually be replaced by the number in the counterForm
+  const handleRandomCountries = () => {
+    if (data) {
+      // This code handes the shuffling of the Data gained from the GRAPHQL
+      const shuffledCountries = [...data.countries].sort(
+        () => 0.5 - Math.random()
+      );
+      const countryCodes = shuffledCountries
+        .slice(0, numCountries)
+        .map((country) => country.code);
+        
 
-  return (
+      //In this part of the code we get all of the specific countries    (Might refactor later for efficiency purposes)
+      Promise.all(
+        countryCodes.map((code) =>
+          fetch(`https://restcountries.com/v2/alpha/${code}`)
+        )
+      )
+        .then((responses) => Promise.all(responses.map((res) => res.json())))
+        .then((countryDetails) => setCountryDetails(countryDetails));
+    }
+  };
+
+  // Will be used in the future on the submit button of the form
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const numCountriesInput = e.currentTarget.elements.namedItem(
+      "numCountries"
+    ) as HTMLInputElement;
+    setNumCountries(Number(numCountriesInput.value));
+    handleRandomCountries();
+  };
+
+  return(
     <>
 
     </>
-  )
-}
+  ) 
+};
+
+export default Home;
