a997c8d4a7aa2edb8ba7c9608a68f922166c60eb
coronado03 <sebascoronado03@hotmail.com>
2023-04-23 15:46:08 +0200

    wrapped img tag in div and reformatted text

--- components/countryCard.tsx
+++ components/countryCard.tsx
@@ -12,18 +12,23 @@ type Props = {
 };
 
 const CountryCard: React.FC<Props> = ({countryName, countryRegion, countrySubregion, countryPopulation, countryCapital, countryLanguages, countryCurrencies, countryFlag }) => {
+ 
+ 
+ 
   return (
       <div className='flex flex-col mt-5 border-4 border-black shadow-md rounded-lg gap-5 w-full'>
         <div className='flex flex-col text-center mt-5'>
           <h1 className='text-5xl'>{countryName}</h1>
-          <img className='w-1/2 mt-4 self-center' src={countryFlag}></img>
+          <div className='w-1/2 self-center mt-4 border border-black'>       
+            <img className='w-fit' src={countryFlag} alt=''></img>
+          </div>
           <h5 className='text-xl'>{countryRegion}, {countrySubregion}</h5>
         </div>
         <div className='mb-5 mx-auto'>   
           <p><b>Capital: </b>{countryCapital}</p>
           <p><b>Population: </b>{countryPopulation.toLocaleString()}</p> 
-          <p><b>Language: </b>{countryLanguages}</p>
-          <p><b>Currencies: </b>{countryCurrencies}</p>
+          <p><b>Language: </b>{countryLanguages.join(", ")}</p>
+          <p><b>Currencies: </b>{countryCurrencies.join(", ")}</p>
         </div>
 
       </div>
